3030f608467ad5ccf5b37f935097d7a2303abc8e
Jairo Emilio Bautista Mora <je.bautista10@isc333.uniandes.edu.co>
2017-02-17 17:05:38 -0500

    Initial Commit

---
+++ HobbyMapTests/HobbyMapTests.swift
@@ -0,0 +1,36 @@
+//
+//  HobbyMapTests.swift
+//  HobbyMapTests
+//
+//  Created by Jairo Emilio Bautista Mora on 2/17/17.
+//  Copyright © 2017 Jairo Emilio Bautista Mora. All rights reserved.
+//
+
+import XCTest
+@testable import HobbyMap
+
+class HobbyMapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
